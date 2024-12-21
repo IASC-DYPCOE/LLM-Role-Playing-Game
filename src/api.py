122545c414be6d43103489df457083e8713779c3
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-21 12:45:50 +0000

    added setter method

--- src/api.py
+++ src/api.py
@@ -4,14 +4,21 @@ from .llm import DuengeonMaster
 
 app = FastAPI()
 
-context = ""
-with open("/home/capybara/code/ml/LLM-Role-Playing-Game/src/prompt.xml", "r") as file:
-    context += file.read().replace("\n", "")
-global_chat_history = [{"System": context}]
-duengeon_master = DuengeonMaster(global_chat_history)
+duengeon_master = DuengeonMaster()
 
 
-@app.get("/dnd")
+@app.get("/dnd/start")
+async def start(request: Request, Response: Response):
+    with open(
+        "/home/capybara/code/ml/LLM-Role-Playing-Game/src/prompt.xml", "r"
+    ) as file:
+        context = file.read().replace("\n", "")
+    global_chat_history = [{"System": context}]
+    duengeon_master.set_global_chat_history(global_chat_history)
+    return {"content": "New game started!"}
+
+
+@app.post("/dnd/play")
 async def home(request: Request, Response: Response, form_input_text: str = Form()):
     input_text = form_input_text
     print(input_text)
--- src/llm.py
+++ src/llm.py
@@ -6,11 +6,13 @@ load_dotenv()
 
 class DuengeonMaster:
     def __init__(self, chat_history=None):
-        self.global_chat_history = chat_history
         self.client = genai.Client()
         self.chat = self.client.chats.create(model="gemini-1.5-flash")
         # self.game_start = self.chat.send_message(str(self.global_chat_history))
         # print(self.game_start.text)
+
+    def set_global_chat_history(self, chat_history):
+        self.global_chat_history = chat_history
 
     def inference(self, input_text):
         self.global_chat_history.append({"User": input_text})
